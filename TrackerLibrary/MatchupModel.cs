be2969aca820dc60a9a4cf4b73c8e8c572bd3b55
Daniela Pais <danielabpais@outlook.pt>
2025-10-06 11:40:52 +0100

    Add project files.

---
+++ TrackerLibrary/MatchupModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class MatchupModel
+    {
+        /// <summary>
+        /// The set of teams that were involved in this match
+        /// </summary>
+        public List<MatchupModel> Entries { get; set; }
+
+        /// <summary>
+        /// The winner of the match
+        /// </summary>
+        public TeamModel Winner { get; set; }
+
+        /// <summary>
+        /// Which round this match is a part of
+        /// </summary>
+        public int MatchupRound { get; set; }
+    }
+}
